48a8c70ae6b45a852640ebf957268b08df128c7d
5m <5m@DESKTOP-EHD8S3P>
2025-09-10 11:02:17 +0200

    Dodaj pliki projektów.

---
+++ analizator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analizator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aplikacja przyjmuje ułamki i wyświetla je 
+
+            wyswietlUlamek(48, 18);
+            wyswietlUlamek(100, 25);
+            wyswietlUlamek(17, 13);
+        }
+        public static void wyswietlUlamek(int licznik, int mianownik)
+        {
+
+            Console.WriteLine("Ułamek: " + licznik + "/" + mianownik);
+            Console.WriteLine("Skrócony: " + licznik/nwd(mianownik, licznik) + "/" + mianownik/nwd(licznik, mianownik));
+            Console.WriteLine("—");
+
+            int nwd(int a, int b)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+                return nwd(b, a % b);
+            }
+            Console.WriteLine(nwd(18, 6));
+
+        }
+    }
+}
